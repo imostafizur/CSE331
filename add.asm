ad7c6297ad4ea69b75159ad7eefb0bb94bcc83ad
Mostafizur Rahman <59665707+imostafizur@users.noreply.github.com>
2022-11-12 00:56:16 +0600

    Assembly

---
+++ add.asm
@@ -0,0 +1,11 @@
+.model small
+.stack 100h
+.data
+.code
+
+main proc
+   
+mov ax, 5
+add ax, 5                      
+   
+main endp
---
+++ multiply.asm
@@ -0,0 +1,13 @@
+.model small
+.stack 100h
+.data
+.code
+
+main proc
+   
+mov ax, 5
+mov bx, 5
+
+mul bx                        
+   
+main endp
